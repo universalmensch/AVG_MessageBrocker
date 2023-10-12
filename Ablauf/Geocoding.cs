e123767a1e099bbd7cb5ce1b8cbc35732d960e1f
melvin1203 <147506157+melvin1203@users.noreply.github.com>
2023-10-12 16:40:43 +0200

    Update Geocoding.cs

--- Ablauf/Geocoding.cs
+++ Ablauf/Geocoding.cs
@@ -4,29 +4,31 @@ using System.Runtime.CompilerServices;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-public class Geocoding {
-     public static async Task Main(string[] args) {
-        List<KeyValuePair<string, string>> geoDaten = await getGeoDaten("Germany","Karlsruhe","40+Moltkestraße");
-        string latitude = geoDaten[0].Value;
-        string longitude = geoDaten[1].Value;
-        Console.WriteLine($"Latitude: {latitude}\nLongitude: {longitude}");
-    }
-    
-    public static async Task<List<KeyValuePair<string, string>>> getGeoDaten(string country, string city, string street) {
-        using var client = new HttpClient();
-        var geoDaten = await client.GetStringAsync($"https://geocode.maps.co/search?country={country}&city={city}&street={street}");
-        //Console.WriteLine(geoDaten);
-        JArray geoArray = JArray.Parse(geoDaten);
-        List<KeyValuePair<string, string>> latundLon = new List<KeyValuePair<string, string>>();
-
-        foreach (JObject geoObject in geoArray) {
-            if (geoObject.ContainsKey("lat") && geoObject.ContainsKey("lon")) {
-                string latitude = geoObject["lat"].ToString();
-                string longitude = geoObject["lon"].ToString();
-                latundLon.Add(new KeyValuePair<string, string>("latitude", latitude));
-                latundLon.Add(new KeyValuePair<string, string>("longitude", longitude));
-            }
+namespace Ablauf.Geocoding {
+    public class Geocoding {
+        public static async Task Main(string[] args) {
+            List<KeyValuePair<string, string>> geoDaten = await getGeoDaten("Germany","Karlsruhe","40+Moltkestraße");
+            string latitude = geoDaten[0].Value;
+            string longitude = geoDaten[1].Value;
+            Console.WriteLine($"Latitude: {latitude}\nLongitude: {longitude}");
         }
-        return latundLon;
+    
+        public static async Task<List<KeyValuePair<string, string>>> getGeoDaten(string country, string city, string street) {
+            using var client = new HttpClient();
+            var geoDaten = await client.GetStringAsync($"https://geocode.maps.co/search?country={country}&city={city}&street={street}");
+            //Console.WriteLine(geoDaten);
+            JArray geoArray = JArray.Parse(geoDaten);
+            List<KeyValuePair<string, string>> latundLon = new List<KeyValuePair<string, string>>();
+
+            foreach (JObject geoObject in geoArray) {
+                if (geoObject.ContainsKey("lat") && geoObject.ContainsKey("lon")) {
+                    string latitude = geoObject["lat"].ToString();
+                    string longitude = geoObject["lon"].ToString();
+                    latundLon.Add(new KeyValuePair<string, string>("latitude", latitude));
+                    latundLon.Add(new KeyValuePair<string, string>("longitude", longitude));
+                }
+            }
+            return latundLon;
+        }
     }
 }
